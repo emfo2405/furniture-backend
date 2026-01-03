213f9a4451f426efec6b919f7145fe62a11680a5
emfo2405 <emfo2405@student.miun.se>
2026-01-03 14:31:20 +0100

    Modeller skapade för kategori, produkt och anställd

---
+++ src/models/employee.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Employee extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  role: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  date: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+
+  constructor(data?: Partial<Employee>) {
+    super(data);
+  }
+}
+
+export interface EmployeeRelations {
+  // describe navigational properties here
+}
+
+export type EmployeeWithRelations = Employee & EmployeeRelations;
---
+++ src/models/product.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Product extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  color: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  stock: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  image: string;
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;

c1d615971ab09040bc345c9046e50d885deb4e13
emfo2405 <emfo2405@student.miun.se>
2026-01-04 13:29:05 +0100

    Loopbacks user-modell tillagd i user.controller för att använda den inbyggda jwt-autentiseringen

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,13 +2,22 @@ import {TokenService} from '@loopback/authentication';
 import {
   MyUserService,
   TokenServiceBindings,
+  User,
   UserRepository,
   UserServiceBindings,
 } from '@loopback/authentication-jwt';
 import {inject} from '@loopback/core';
-import {repository} from '@loopback/repository';
+import {model, property, repository} from '@loopback/repository';
 import {SecurityBindings, UserProfile} from '@loopback/security';
 
+@model()
+export class NewUserRequest extends User {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+}
 
 export class UserController {
   constructor(
@@ -20,4 +29,5 @@ export class UserController {
     public user: UserProfile,
     @repository(UserRepository) protected userRepository: UserRepository,
   ) { }
+
 }

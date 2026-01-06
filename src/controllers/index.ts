744633430e0f4018da9fccc7fffc07306e27ef52
emfo2405 <emfo2405@student.miun.se>
2026-01-06 13:15:10 +0100

    employee borttaget

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,7 @@
-export * from './ping.controller';
-export * from './employee.controller';
-export * from './category.controller';
-export * from './product.controller';
 export * from './category-product.controller';
+export * from './category.controller';
+export * from './ping.controller';
 export * from './product-category.controller';
+export * from './product.controller';
 export * from './user.controller';
+

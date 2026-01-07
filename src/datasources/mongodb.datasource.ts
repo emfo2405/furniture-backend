42e14a7e190f6cedd748dbe026e8ceb1a30517a4
emfo2405 <emfo2405@student.miun.se>
2026-01-07 10:13:58 +0100

    Fel fixade för databasanslutning

--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -1,5 +1,7 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const config = {
   name: 'mongodb',
